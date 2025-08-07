788151c9be43502b91718a09577ec7bef7a0ef55
Silas <kammersi.to@gmail.com>
2025-08-08 00:15:21 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 export const CONFIG = {
-    API_URL: 'http://localhost:3000/api',
-    //API_URL: 'https://booking-production-3ae3.up.railway.app/api',
+    //API_URL: 'http://localhost:3000/api',
+    API_URL: 'https://booking-production-3ae3.up.railway.app/api',
 };
 
 export const placeholderImages = [{
     src: 'src/assets/ImagePlaceholder.png',
     alt: 'Platzhalterbild'
+
 }];
